f82870fd8a59cda466e9e6e9c2c9af4855021b0b
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-26 22:59:06 +0600

    courses page updated

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import SingleCourse from '../SingleCourse/SingleCourse';
 
 const Courses = () => {
   return (
-    <div>
-      <div className="w-full lg:w-1/4"></div>
-      <div className="w-full lg:w-3/4 grid grid-cols-2 gap-10 m-10"></div>
+    <div className="flex flex-col lg:flex-row m-4">
+      <div className="w-full lg:w-1/4 flex flex-col">
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 mb-2">
+          Responsive
+        </button>
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 mb-2">
+          Responsive
+        </button>
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-4 mb-2">
+          Responsive
+        </button>
+      </div>
+      <div className="w-full lg:w-3/4 grid grid-cols-1 lg:grid-cols-2 gap-10 p-4">
+        <SingleCourse></SingleCourse>
+        <SingleCourse></SingleCourse>
+        <SingleCourse></SingleCourse>
+        <SingleCourse></SingleCourse>
+        <SingleCourse></SingleCourse>
+        <SingleCourse></SingleCourse>
+      </div>
     </div>
   );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyB_ai0IeorYHDHd2YiLHiqTeJFa4lQKBhY',
+  authDomain: 'muzic-client-side-13849.firebaseapp.com',
+  projectId: 'muzic-client-side-13849',
+  storageBucket: 'muzic-client-side-13849.appspot.com',
+  messagingSenderId: '356891112595',
+  appId: '1:356891112595:web:9ffe3fbcb5f30a56b2c966',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
